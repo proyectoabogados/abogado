0a1bf65e8d72f153e0c8cca8421843d279c99aa0
knarf20 <frankalvarado1995@gmail.com>
2019-06-08 18:28:18 -0400

    add redux and config store and history routes

---
+++ src/components/Root.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
+import { Route, Router } from 'react-router-dom'
+import ScrollToTop from 'components/ScrollToTop'
+import App from './App'
+
+export default class Root extends Component {
+  render() {
+    const { store, history } = this.props
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <ScrollToTop>
+            <Route path="/" component={App} />
+          </ScrollToTop>
+        </Router>
+      </Provider>
+    )
+  }
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import Root from 'components/Root'
+import configureStore, { history } from './store/configureStore'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+render(<Root store={store} history={history} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
+
+if (module.hot) {
+  module.hot.accept('components/Root', () => {
+    const NextApp = Root.default
+    render(<NextApp />, document.getElementById('root'))
+  })
+}
