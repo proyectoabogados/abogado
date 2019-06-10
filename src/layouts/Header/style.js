6e98af11bd1d0290054e3702a8af9150e799b9a2
knarf20 <frankalvarado1995@gmail.com>
2019-06-10 01:03:49 -0400

    add header componet

---
+++ src/layouts/Header/style.js
@@ -0,0 +1,35 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const drawerWidth = 240
+
+const style = makeStyles(theme => ({
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+export default style
