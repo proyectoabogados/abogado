e7cc5f1cdce3ec39b60dff52d1db80ce805ab0bc
knarf20 <frankalvarado1995@gmail.com>
2019-06-14 22:01:56 -0400

    menu test

--- src/common/Menu.js
+++ src/common/Menu.js
@@ -3,9 +3,57 @@ import App from 'components/App'
 
 const Menu = () => [
   {
+    name: 'Marketing',
+    icon: DashboardIcon,
+    path: 'marketing',
+    component: App,
+  },
+  {
+    name: 'Capacity',
+    icon: DashboardIcon,
+    path: 'capacity',
+    component: App,
+  },
+  {
+    name: 'Experience',
+    icon: DashboardIcon,
+    path: 'experience',
+    component: App,
+  },
+  {
+    name: 'Staff',
+    icon: DashboardIcon,
+    path: 'staff',
+    component: App,
+  },
+  {
+    name: 'Staffing',
+    icon: DashboardIcon,
+    path: 'staffing',
+    component: App,
+  },
+  {
+    name: 'Salaries',
+    icon: DashboardIcon,
+    path: 'salaries',
+    component: App,
+  },
+  {
     name: 'Market',
     icon: DashboardIcon,
-    path: 'url',
+    path: 'market',
+    component: App,
+  },
+  {
+    name: 'Quality',
+    icon: DashboardIcon,
+    path: 'quality',
+    component: App,
+  },
+  {
+    name: 'Finances',
+    icon: DashboardIcon,
+    path: 'finances',
     component: App,
   },
 ]
